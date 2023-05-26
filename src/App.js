1b4d741112022afd6fce484e58bdfe31c52d7257
germaimo <figueroa.german@maimonidesvirtual.com.ar>
2023-05-26 18:22:13 -0300

    basicon, llamadas a api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Home() {
+
+const [loading, setLoading] = useState(true);
+const [audioTracks, setAudioTracks] = useState([]);
+const [midiTracks, setMidiTracks] = useState([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const audioResponse = await fetch('http://localhost:3000/audio-tracks');
+      const audioData = await audioResponse.json();
+      setAudioTracks(audioData);
+
+      const midiResponse = await fetch('http://localhost:3000/midi-tracks');
+      const midiData = await midiResponse.json();
+      setMidiTracks(midiData);
+
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+return (
+  <div>
+    {loading ? (
+      <p>Cargando...</p>
+    ) : (
+      <>
+        <h2>Pistas de audio:</h2>
+        {audioTracks.map((track, index) => (
+          <div key={index}>
+            <p>{track}</p>
+            <textarea rows="4" cols="50" placeholder="Agregar texto aquí"></textarea>
+          </div>
+        ))}
+
+        <h2>Pistas MIDI:</h2>
+        {midiTracks.map((track, index) => (
+          <div key={index}>
+            <p>{track}</p>
+            <textarea rows="4" cols="50" placeholder="Agregar texto aquí"></textarea>
+          </div>
+        ))}
+      </>
+    )}
+  </div>
+);
+
 }
-
-export default App;
