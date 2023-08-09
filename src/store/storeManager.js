5af6285960519d1f2be15802b4433cf8f1f49a97
germaimo <figueroa.german@maimonidesvirtual.com.ar>
2023-08-09 13:41:32 -0300

    reducers separados, uno para el manejo de un proyecto, otro para el manejo de multiples proyectos, esto es mas que nada para el listado inicial

--- src/store/storeManager.js
+++ src/store/storeManager.js
@@ -1,5 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './reducer';
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import singleProyectReducer from "./singleProyectReducer";
+import proyectsReducer from "./proyectsReducer";
+
+const rootReducer = combineReducers({
+  singleProject: singleProyectReducer,
+  multipleProjects: proyectsReducer,
+});
+
 export default configureStore({
-    reducer
+  rootReducer,
 });
