c6e1d97917019a4c18d40e5522718bc8b5f286c9
germaimo <figueroa.german@maimonidesvirtual.com.ar>
2023-08-16 13:52:18 -0300

    anadido paquete simulacion tipeo, anadido validacion y simulacion en proyecto.js

--- src/routes/Proyecto.js
+++ src/routes/Proyecto.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import * as typeActions from "../store/tipoAcciones";
+import { TypeAnimation } from "react-type-animation";
 
 const Proyecto = () => {
   const audioTracks = useSelector((state) => state.audioTracks);
   const midiTracks = useSelector((state) => state.midiTracks);
   const nombreProyecto = useSelector((state) => state.nombreProyecto);
   const [usuarios, setUsuarios] = useState([]);
+  const [showTextarea, setShowTextarea] = useState(true);
+  const [showSimulacra, setShowSimulacra] = useState(false);
 
+  const [showTextareaAudio, setShowTextareaAudio] = useState(true);
+  const [showSimulacraAudio, setShowSimulacraAudio] = useState(false);
+ 
+  
   const dispatch = useDispatch();
 
   const guardarComentario = (tipo, trackName, e) => {
@@ -37,8 +44,22 @@ const Proyecto = () => {
   }, []);
 
   useEffect(() => {
-    console.log(usuarios);
-  }, [usuarios]);
+    const timeout = setTimeout(() => {
+      setShowTextarea(false);
+      setShowSimulacra(true);
+    }, 6000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowTextareaAudio(false);
+      setShowSimulacraAudio(true);
+    }, 7500);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
     <div>
@@ -65,7 +86,13 @@ const Proyecto = () => {
 
         <ul>
           {usuarios.map((usuario, index) => (
-            <li key={index}> <p className={`usuarioLoggeado small color-${index} `}> {usuario} </p> </li>
+            <li key={index}>
+              {" "}
+              <p className={`usuarioLoggeado small color-${index} `}>
+                {" "}
+                {usuario}{" "}
+              </p>{" "}
+            </li>
           ))}
           <li>
             <p className="usuarioLoggeado">GF</p>
@@ -106,15 +133,43 @@ const Proyecto = () => {
                   <p>{item.track}</p>
                 </div>
                 <div className="containerTextarea">
-                  <textarea
+                {(index === 1 && showTextareaAudio) && <textarea
+                      id={`midi-${index}`}
+                      rows="4"
+                      cols="50"
+                      placeholder="Agregar texto aquí"
+                      defaultValue={item.text}
+                      onBlur={(e) => guardarComentario("audio", item.track, e)}
+                    /> }
+
+{(index === 1 && showSimulacraAudio) && <TypeAnimation
+                      sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        "Este canal esta bueno, podris",
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        "Este canal esta bueno, podrias reemplaz",
+                        1000,
+                        "Este canal esta bueno, podrias fusionarlo con el canal de abajo",
+                        1000,
+                      ]}
+                      wrapper="textarea"
+                      style={{height: "4.5em", width: "98%"}}
+                      speed={25}
+                      repeat={0}
+                    /> }
+
+                  {index !== 1 && <textarea
                     id={`audio-${index}`}
                     rows="4"
                     cols="50"
                     placeholder="Agregar texto aquí"
                     defaultValue={item.text}
                     onBlur={(e) => guardarComentario("audio", item.track, e)}
-                  />
-                  {index === 1 && <div className="circuloUsuarioLR hidden-item2">LR</div>}
+                  />}
+                  
+                  {index === 1 && (
+                    <div className="circuloUsuarioLR hidden-item2">LR</div>
+                  )}
                   <div className="circuloUsuario">GF</div>
                 </div>
               </div>
@@ -151,15 +206,45 @@ const Proyecto = () => {
                   <p>{item.track}</p>
                 </div>
                 <div className="containerTextarea">
-                  <textarea
-                    id={`midi-${index}`}
-                    rows="4"
-                    cols="50"
-                    placeholder="Agregar texto aquí"
-                    defaultValue={item.text}
-                    onBlur={(e) => guardarComentario("midi", item.track, e)}
-                  />
-                  {index === 0 && <div className="circuloUsuarioTM hidden-item1">TM</div>}
+
+                  {(index === 0 && showTextarea) && <textarea
+                      id={`midi-${index}`}
+                      rows="4"
+                      cols="50"
+                      placeholder="Agregar texto aquí"
+                      defaultValue={item.text}
+                      onBlur={(e) => guardarComentario("midi", item.track, e)}
+                    /> }
+
+
+                  {(index === 0 && showSimulacra) && <TypeAnimation
+                      sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        "Este canal esta bueno, podris",
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        "Este canal esta bueno, podrias reemplaz",
+                        1000,
+                        "Este canal esta bueno, podrias fusionarlo con el canal de abajo",
+                        1000,
+                      ]}
+                      wrapper="textarea"
+                      style={{height: "4.5em", width: "98%"}}
+                      speed={25}
+                      repeat={0}
+                    /> }
+
+                    {index !== 0 && <textarea
+                      id={`midi-${index}`}
+                      rows="4"
+                      cols="50"
+                      placeholder="Agregar texto aquí"
+                      defaultValue={item.text}
+                      onBlur={(e) => guardarComentario("midi", item.track, e)}
+                    /> }
+
+                  {index === 0 && (
+                    <div className="circuloUsuarioTM hidden-item1">TM</div>
+                  )}
                   <div className="circuloUsuario">GF</div>
                 </div>
               </div>
