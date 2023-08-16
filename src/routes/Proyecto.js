54a98f71fc7cc10ee2ef7f64aa97119fb9bf2f8b
germaimo <figueroa.german@maimonidesvirtual.com.ar>
2023-08-16 12:47:26 -0300

    agregado simulacion de usuarios

--- src/routes/Proyecto.js
+++ src/routes/Proyecto.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import * as typeActions from "../store/tipoAcciones";
@@ -7,6 +7,7 @@ const Proyecto = () => {
   const audioTracks = useSelector((state) => state.audioTracks);
   const midiTracks = useSelector((state) => state.midiTracks);
   const nombreProyecto = useSelector((state) => state.nombreProyecto);
+  const [usuarios, setUsuarios] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -26,32 +27,49 @@ const Proyecto = () => {
     }
   };
 
+  useEffect(() => {
+    // Simular usuarios que entran después de 5 segundos
+    const userEntryTimeout = setTimeout(() => {
+      setUsuarios(["LR", "TM"]); // Agregar usuarios simulados al array
+    }, 5000);
+
+    return () => clearTimeout(userEntryTimeout);
+  }, []);
+
+  useEffect(() => {
+    console.log(usuarios);
+  }, [usuarios]);
+
   return (
     <div>
-      
       <nav>
-       <Link to={{ pathname: "/" }}>
-        <svg
-          width="60"
-          height="60"
-          viewBox="0 0 60 60"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g clip-path="url(#clip0_6_2314)">
-            <rect width="60" height="60" fill="#A9A9A9" />
-            <path d="M30 12L14 24V48H24V34H36V48H46V24L30 12Z" fill="black" />
-          </g>
-          <defs>
-            <clipPath id="clip0_6_2314">
-              <rect width="60" height="60" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
+        <Link to={{ pathname: "/" }}>
+          <svg
+            width="60"
+            height="60"
+            viewBox="0 0 60 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_6_2314)">
+              <rect width="60" height="60" fill="#A9A9A9" />
+              <path d="M30 12L14 24V48H24V34H36V48H46V24L30 12Z" fill="black" />
+            </g>
+            <defs>
+              <clipPath id="clip0_6_2314">
+                <rect width="60" height="60" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
         </Link>
 
         <ul>
-          <li><p className="usuarioLoggeado">GF</p></li>
+          {usuarios.map((usuario, index) => (
+            <li key={index}> <p className={`usuarioLoggeado small color-${index} `}> {usuario} </p> </li>
+          ))}
+          <li>
+            <p className="usuarioLoggeado">GF</p>
+          </li>
         </ul>
       </nav>
 
@@ -69,25 +87,25 @@ const Proyecto = () => {
                 <div className="nombreTrackAudio">
                   <span>
                     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="34"
-      height="34"
-      fillRule="evenodd"
-      strokeLinejoin="round"
-      strokeMiterlimit="2"
-      clipRule="evenodd"
-      viewBox="0 0 64 64"
-    >
-      <path fill="none" d="M-1024-192H256v800h-1280z"></path>
-      <path
-        fillRule="nonzero"
-        d="M8.002 43.851h47.822v5.992H8.002zM8.212 28.93h47.822v5.991H8.212zM8.212 14.008h47.822V20H8.212z"
-      ></path>
-    </svg>
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="34"
+                      height="34"
+                      fillRule="evenodd"
+                      strokeLinejoin="round"
+                      strokeMiterlimit="2"
+                      clipRule="evenodd"
+                      viewBox="0 0 64 64"
+                    >
+                      <path fill="none" d="M-1024-192H256v800h-1280z"></path>
+                      <path
+                        fillRule="nonzero"
+                        d="M8.002 43.851h47.822v5.992H8.002zM8.212 28.93h47.822v5.991H8.212zM8.212 14.008h47.822V20H8.212z"
+                      ></path>
+                    </svg>
                   </span>
                   <p>{item.track}</p>
                 </div>
-                <div class="containerTextarea">
+                <div className="containerTextarea">
                   <textarea
                     id={`audio-${index}`}
                     rows="4"
@@ -96,9 +114,9 @@ const Proyecto = () => {
                     defaultValue={item.text}
                     onBlur={(e) => guardarComentario("audio", item.track, e)}
                   />
-                  <div class="circuloUsuario">GF</div>
+                  {index === 1 && <div className="circuloUsuarioLR hidden-item2">LR</div>}
+                  <div className="circuloUsuario">GF</div>
                 </div>
-
               </div>
             ))}
         </div>
@@ -132,14 +150,18 @@ const Proyecto = () => {
                   </span>
                   <p>{item.track}</p>
                 </div>
-                <textarea
-                  id={`midi-${index}`}
-                  rows="4"
-                  cols="50"
-                  placeholder="Agregar texto aquí"
-                  defaultValue={item.text}
-                  onBlur={(e) => guardarComentario("midi", item.track, e)}
-                />
+                <div className="containerTextarea">
+                  <textarea
+                    id={`midi-${index}`}
+                    rows="4"
+                    cols="50"
+                    placeholder="Agregar texto aquí"
+                    defaultValue={item.text}
+                    onBlur={(e) => guardarComentario("midi", item.track, e)}
+                  />
+                  {index === 0 && <div className="circuloUsuarioTM hidden-item1">TM</div>}
+                  <div className="circuloUsuario">GF</div>
+                </div>
               </div>
             ))}
         </div>
